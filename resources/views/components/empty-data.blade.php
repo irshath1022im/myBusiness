34fe6e79042061c73e4b33e0b76a8af1d735593b
irshath1022im <im.irshath1022@gmail.com>
2022-08-15 14:17:08 +0300

    customer form

---
+++ resources/views/components/empty-data.blade.php
@@ -0,0 +1,10 @@
+
+ <div class="card">
+    <div class="card-body">
+
+        <div class="alert alert-warning" role="alert">
+            <strong>{{ $message }}</strong>
+        </div>
+
+    </div>
+</div>
--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -1,12 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-   
-    <div class="list-group">
-        @foreach ($customers as $item)
-        <a href={{ route('customers.show', ['customer' => $item->id]) }} class="list-group-item list-group-item-action ">{{ $item->customer }}</a>
-            
-        @endforeach
+
+<div class="card">
+    <div class="card-header">
+        <div class="card-title ">CUSTOMERS</div>
     </div>
+    <div class="card-body">
+
+
+    @if ( $customers->isEmpty())
+        @component('components.empty-data',['message' => 'No Customers Found..'])
+
+        @endcomponent
+    @endif
+
+
+
+
+
+    <div class="row">
+
+        @foreach ($customers as $customer)
+
+
+                <div class="col-md-4">
+
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <h3 class="card-title text-uppercase bg-info p-3">{{ $customer->customer }}</h3>
+                            <img class="card-img-top img-fluid" src="/images/customer.jpg" alt="Card image cap">
+                        </div>
+
+                        <div class="card-footer">
+                            <button class="btn btn-success">
+                                    VISAS <span class="badge bg-primary">15</span>
+                            </button>
+
+                            <button class="btn btn-success">
+                                ACCOUNTS <span class="badge bg-primary">15</span>
+                        </button>
+                        </div>
+
+                    </div>
+
+                </div>
+          @endforeach
+
+    </div>
+
+
+</div>
+</div>
+
+
 
 @endsection
